114243f5705b122252d47d6ac1e546819fc6ce3a
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-09-13 18:48:22 +0500

    Task1

---
+++ Tyuiu.NikiforovFA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.NikiforovFA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Федор";
+            int age = 18;
+            Console.WriteLine("Здравстуйте, меня зовут " + name);
+            Console.WriteLine("И мне " + age + " лет");
+            Console.ReadKey();
+            
+        }
+    }
+}
